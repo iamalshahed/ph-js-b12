827b81cb81667d4d2639cf7ace4dc8624c9fe2a6
iamalshahed <shahedevise@gmail.com>
2025-08-08 15:54:59 +0600

    parameter handle

---
+++ 06. js_function/parameter.js
@@ -0,0 +1,33 @@
+function vatKhawa() {
+  console.log("hat dhow");
+  console.log("boso");
+  console.log("plate e khabar dabar naw");
+  console.log("gopat gpat kore khawa suru koro");
+}
+
+// vatKhawa();
+
+// borgo
+
+function square(num) {
+  console.log(num);
+  const borgo = num * num;
+  console.log("borgo: " + borgo);
+}
+square(4);
+console.log("--------------");
+square(6);
+console.log("--------------");
+square(16);
+
+function jog(num1, num2) {
+  const total = num1 + num2;
+  console.log(total);
+}
+jog(2, 5);
+
+function multiple(a, b, c, d, e) {
+  const sum = a + b + c + d + e;
+  console.log(sum);
+}
+multiple(1, 2, 5, 9, 45);
