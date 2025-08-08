94f5fce3b7572810ee692483d87737f5969773c1
iamalshahed <shahedevise@gmail.com>
2025-08-08 14:41:51 +0600

    function syntax

---
+++ 06. js_function/function.js
@@ -0,0 +1,14 @@
+function fanOffKor(){
+    console.log('bosa theke uthe dara');
+    console.log('click the switch to off the fan');
+}
+// call the function
+fanOffKor();
+
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min goso and goso and fena felo');
+}
+// call the function
+brushTeeth();
