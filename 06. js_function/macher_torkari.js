d2df845638a9c1a92bfae21a2d6f4f9add2d6bfb
iamalshahed <shahedevise@gmail.com>
2025-08-08 15:22:36 +0600

    function with visual effect

---
+++ 06. js_function/macher_torkari.js
@@ -0,0 +1,13 @@
+function macherTorkari(mach, sobji, moshla) {
+  // রান্নার নির্দেশনাবলী
+  console.log("তেল গরম করো");
+  console.log(moshla + " ফোরান");
+  console.log(sobji + " দিন");
+  console.log(mach + " দিন");
+
+  // return
+  return "susadhu macher torkari";
+}
+
+const result = macherTorkari('ইলিশ', 'আলু', 'হলুদ');
+console.log(result);
