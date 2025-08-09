f57f016f3d971240ea14a1d5afb673ff02084ee2
iamalshahed <shahedevise@gmail.com>
2025-08-09 21:23:05 +0600

    first task done

---
+++ 06. js_function/function_practice_task/task-1.js
@@ -0,0 +1,11 @@
+/**
+ * Task-1
+Take four parameters. Multiply the four numbers and then return the result
+ */
+
+function multiply(num1, num2, num3, num4) {
+  const gunfol = num1 * num2 * num3 * num4;
+  return gunfol;
+}
+const input = multiply(5, 2, 90, 45);
+console.log(input);
