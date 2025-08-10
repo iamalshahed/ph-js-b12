e72a538e56c945fd558e018e7cd9399d9d11cade
iamalshahed <shahedevise@gmail.com>
2025-08-10 17:52:36 +0600

    Math, abs, round, ceil, floor, and random number

---
+++ 07. js_problem_part_1/math.js
@@ -0,0 +1,26 @@
+// console.log(Math.max);
+
+const mmin = Math.min(45, 21, 65, 99, 126, 5, -99);
+console.log(mmin);
+
+const max = Math.max(45, 21, 65, 99, 126, 5, -99, 987);
+console.log(max);
+
+let piMan = Math.PI;
+console.log(piMan);
+
+console.log(Math.abs(5 - 10));
+
+console.log(Math.round(8.25));
+console.log(Math.round(8.85));
+
+console.log(Math.floor(5.9));
+console.log(Math.ceil(5.8));
+console.log(Math.ceil(5.00001));
+
+// math.random()
+
+console.log(Math.random());
+console.log(Math.random() * 10);
+const rand = Math.random() * 1000;
+console.log(Math.round(rand));
