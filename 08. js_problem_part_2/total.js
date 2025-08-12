53903743e3de62dcbeab8f8c4692d77c86698874
iamalshahed <shahedevise@gmail.com>
2025-08-12 14:24:54 +0600

    21-5 array of obejct er modde theke sokol number er jogfol ber korar

---
+++ 08. js_problem_part_2/total.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    name: "shampo",
+    price: 300,
+  },
+  {
+    name: "chiruni",
+    price: 100,
+  },
+  {
+    name: "shirt",
+    price: 700,
+  },
+  {
+    name: "pant",
+    price: 1200,
+  },
+];
+
+function getShoppingTotal(products) {
+  let total = 0;
+  for(const pro of products){
+    total += pro.price;
+  }
+  return total;
+}
+const total = getShoppingTotal(products);
+console.log("total ajke khosabe:", total);
