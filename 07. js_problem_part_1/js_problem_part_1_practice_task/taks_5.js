92afcc90d6098ad6617bbdd4bcdd44a62e418697
iamalshahed <shahedevise@gmail.com>
2025-08-14 15:23:14 +0600

    task-5 done

---
+++ 07. js_problem_part_1/js_problem_part_1_practice_task/taks_5.js
@@ -0,0 +1,10 @@
+/**
+ * Task-5:
+ * Generate a random number between 10 to 20.
+ */
+
+function generateRandomNumber(num){
+    const math = Math.random() * 11;
+    return math;
+}
+console.log(generateRandomNumber());
