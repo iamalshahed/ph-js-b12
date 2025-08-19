bafd3eacca72f4f2bbebfeccd85193cb47bba7b1
iamalshahed <shahedevise@gmail.com>
2025-08-20 01:23:06 +0600

    Add onclick handler directly or via javascript

--- 11. dom_events/js/event.js
+++ 11. dom_events/js/event.js
@@ -1,1 +1,25 @@
 console.log("extarnal file");
+
+// option 2 to hanlde js click event
+function makeYellow() {
+  document.body.style.background = "yellow";
+  document.body.style.color = "#000";
+}
+
+function makeRed() {
+  document.body.style.background = "red";
+  document.body.style.color = "#fff";
+}
+
+function makeBlack() {
+  document.body.style.background = "black";
+  document.body.style.color = "#fff";
+}
+
+// option 3 getElementById and then set onclick
+const btnMakeBlue = document.getElementById("btn_make_blue");
+// console.log(btnMakeBlue)
+btnMakeBlue.onclick = function makeBlue() {
+  document.body.style.background = "blue";
+  document.body.style.color = "#fff";
+};
