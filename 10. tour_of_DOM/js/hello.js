41d823d9a0f4af1ad66a59ef0f8f320b829874d0
iamalshahed <shahedevise@gmail.com>
2025-08-19 11:40:30 +0600

    Traversing dom - getElementsByClass VS getElementById

--- 10. tour_of_DOM/js/hello.js
+++ 10. tour_of_DOM/js/hello.js
@@ -2,18 +2,34 @@ const mySelf = {
   name: "Shahed",
   age: 17,
   myFunc: function (experience) {
-    console.log("my experience: ", experience);
+    // console.log("my experience: ", experience);
   },
 };
 
 const listCollection = document.getElementsByTagName("li");
-console.log(listCollection);
+// console.log(listCollection);
 
 for (let list of listCollection) {
-  console.log(list);
+  //   console.log(list.innerText);
 }
 
 const headingEelm = document.getElementsByTagName("h1");
 for (let heading of headingEelm) {
-  console.log(heading);
+  //   console.log(heading.innerText);
+}
+
+// get element by id
+const foodH1 = document.getElementById("food_title");
+console.log(foodH1.innerText);
+foodH1.innerText = "foods i love it";
+
+let band = document.getElementById("band");
+band.innerText = "rachel kaka.. jalali set";
+
+// get element by className
+let foreign = document.getElementsByClassName("foreign");
+console.log(foreign);
+// loop through
+for (let item of foreign) {
+  console.log(item.innerText);
 }
