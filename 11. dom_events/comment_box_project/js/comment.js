7170651152ddb735a9fb904325e9885cc5ed34ee
iamalshahed <shahedevise@gmail.com>
2025-08-20 11:05:43 +0600

    Create a comment box and display comment (DOM Project)

---
+++ 11. dom_events/comment_box_project/js/comment.js
@@ -0,0 +1,24 @@
+const commentBoxContainer = document.getElementById("comment__box__container");
+const commentBox = document.getElementById("comment__box");
+const btnComment = document.getElementById("btn__comment");
+
+// step: 1
+btnComment.addEventListener("click", function () {
+  if (commentBox.value.trim() !== "") {
+    // step: 2
+    const newComment = commentBox.value.trim();
+
+    // step: 3
+    const postCmt = document.createElement("p");
+    postCmt.classList.add("cmt");
+    postCmt.innerText = newComment;
+
+    // step: 4
+    commentBoxContainer.appendChild(postCmt);
+
+    // step: 5
+    commentBox.value = "";
+  } else {
+    alert("Comment field id empty!");
+  }
+});
