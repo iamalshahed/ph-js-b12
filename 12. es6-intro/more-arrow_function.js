faab5a7acbe6ef37d67fec68bbb66cab3a4fe9c0
iamalshahed <shahedevise@gmail.com>
2025-08-30 17:12:18 +0600

    29-5 More Arrow functions and arrow functions =>

---
+++ 12. es6-intro/more-arrow_function.js
@@ -0,0 +1,30 @@
+const getTax = (ammount, taxRate) => (ammount * taxRate) / 100;
+const add = (p, q) => p + q;
+
+const getSquare = (x) => x * x;
+console.log(getSquare(8));
+
+const getHalf = (a) => a / 2;
+console.log(getHalf(2));
+
+// get first element
+const getFirstElm = (nums) => nums[0];
+console.log(getFirstElm([82, 15, 67, 42]));
+
+// annonymous function
+document.getElementById("").addEventListener("click", () => {
+  // code
+});
+
+// annonymous function with parameter
+document.getElementById("").addEventListener("click", (e) => {
+  // code
+});
+
+/*
+
+() => {
+  // code
+}
+
+*/
