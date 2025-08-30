dbe1091f11c042ce99ac653290b55725f675828b
iamalshahed <shahedevise@gmail.com>
2025-08-30 14:44:47 +0600

    ES6 Intro difference between var, Let, and const

---
+++ 12. es6-intro/let_const.js
@@ -0,0 +1,21 @@
+// es6
+const name = "Bangladesh";
+const countryName = "bd" + " " + name;
+console.log(countryName);
+
+let price = 120;
+price = 150;
+console.log(price);
+
+const myarr = ["shahed", "pakkar-sing", "voda"];
+myarr.push("ohsna");
+console.log(myarr);
+
+// object
+const myobj = {
+  name: "shahed",
+  age: 17,
+};
+myobj.student = true;
+myobj.age = 20;
+console.log(myobj);
