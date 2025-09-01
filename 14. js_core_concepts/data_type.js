5446405ea82420347f3528bee0b84d3ecdd7f570
iamalshahed <shahedevise@gmail.com>
2025-09-01 14:44:48 +0600

    31-1 Module Overview, Primitive and non-primitive data type

---
+++ 14. js_core_concepts/data_type.js
@@ -0,0 +1,28 @@
+let data = 4;
+data = "me";
+data = false;
+data = {
+  nme: "vangari",
+  price: 40,
+};
+
+data = [5];
+data = undefined;
+data = null;
+
+console.log(data);
+
+// js: primitive data type
+const price = 50;
+const name = "bodrul";
+const isPoor = false;
+let isKnows = undefined;
+
+// non primitive data type
+const student = {
+  name: "badal",
+  location: "bristi goli",
+  subject: "botany",
+};
+
+const friends = ["shahed", "bokchoy", "oshan", "jihad kaka"];
