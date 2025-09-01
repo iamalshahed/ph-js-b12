3df1a5ba1a07dedfd9e5a979f132854d931cf3ac
iamalshahed <shahedevise@gmail.com>
2025-09-01 16:44:04 +0600

    31-4 double equal (==) vs triple equal (===), implicit conversion

---
+++ 14. js_core_concepts/double_equal.js
@@ -0,0 +1,9 @@
+console.log(2 == 2);
+console.log(2 == "2");
+console.log(2 == "12");
+console.log(true == 1);
+console.log(false == 0);
+console.log(true == "1");
+console.log(false == "0");
+console.log(null == undefined);
+console.log(NaN == NaN);
---
+++ 14. js_core_concepts/triple_equal.js
@@ -0,0 +1,12 @@
+/**
+ * === first check type. if type is different, they are different.
+ * if type is same, then checks the value. if value is same --->> true or if value is different --->> false
+ */
+
+console.log(2 === 2);
+console.log(2 === "2");
+console.log(1 === 2);
+console.log(true === 1);
+console.log(false === 0);
+console.log(null === undefined);
+console.log(0 === "");
