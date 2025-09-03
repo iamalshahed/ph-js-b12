7d0c0c62a236455c892192598b052060e1e62436
iamalshahed <shahedevise@gmail.com>
2025-09-03 13:29:44 +0600

    32-2 Recap API Concept , Intro to JSON

---
+++ 15. api_json_data_load/script.js
@@ -0,0 +1,23 @@
+console.log("Explore API");
+
+const person = {
+  name: "Shahed",
+  fruit: "dalim",
+  dish: "halim",
+  friends: ["oshan", "bokchoy", "mazharul", "mewoya"],
+  isRich: false,
+  money: 320000,
+};
+console.log(person, typeof person);
+
+/**
+ * JSON --->> JavaScript Object with notation
+ * JSON.stringify()
+ * JSON.parse()
+ */
+
+const personJSON = JSON.stringify(person);
+console.log(personJSON, typeof personJSON);
+
+const parseJSON = JSON.parse(personJSON);
+console.log(parseJSON);
