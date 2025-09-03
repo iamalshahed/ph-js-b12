d95c67c292bf793ad81feb32c2bb03a4c7c66f04
iamalshahed <shahedevise@gmail.com>
2025-09-03 18:25:07 +0600

    32-6 Display Post Card & Style using CSS

--- 15. api_json_data_load/post_title.js
+++ 15. api_json_data_load/post_title.js
@@ -1,27 +1,27 @@
 // select elements
-const dptc = document.getElementById("dptc");
+const cardContainer = document.getElementById("card_container");
 
-const loadPostTitle = () => {
+const loadAllPosts = () => {
   const url = "https://jsonplaceholder.typicode.com/posts";
 
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      // console.log(data);
-      displayPostTitle(data);
+      dpAllPost(data);
     });
 };
 
-const displayPostTitle = (posts) => {
-  // empty the container
-  dptc.innerHTML = "";
+const dpAllPost = (posts) => {
+  cardContainer.innerHTML = "";
 
   posts.forEach((post) => {
-    // 1. create an element
-    let li = document.createElement("li");
-    li.innerText = post.title;
-
-    // 2. appendChild
-    dptc.appendChild(li);
+    let div = document.createElement("div");
+    div.innerHTML = `<div class="p-4 bg-gray-100 rounded-md border border-gray-200 h-full space-y-2">
+            <h2 class="text-2xl font-semibold">${post.title}</h2>
+            <p class="text-zinc-900 text-justify">${post.body}</p>
+        </div>`;
+    cardContainer.appendChild(div);
   });
 };
+
+loadAllPosts();
